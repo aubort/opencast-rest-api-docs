04a088b957c86766193161d37a3ccbbe67cfcdd9
Pascal Aubort <pascal@entwinemedia.com>
2015-04-22 21:00:24 +0200

    small fixes

--- jar/build.sh
+++ jar/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+cat ../template.html > ui/restdocs/template.xhtml
+
 jar vfu ~/workspace/matterhorn-config/vagrant/share/matterhorn/lib/matterhorn/matterhorn-common-2.1-SNAPSHOT.jar ui/
 
 entutils matterhorn restart
